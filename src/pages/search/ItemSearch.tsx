50647f715239b4a4fc3c02614971537689d46136
llwodnjs <llwodnjs@naver.com>
2023-02-04 11:38:10 +0900

    [추가] 아이템 조회페이지 추가
    [추가] 아이템 조회페이지 추가

--- src/pages/search/ItemSearch.tsx
+++ src/pages/search/ItemSearch.tsx
@@ -2,6 +2,8 @@ import "@/assets/scss/pages/search/itemsearch.style.scoped.scss";
 const itemImage = require("@/assets/images/itemImage.png");
 const diamondImage = require("@/assets/images/diamond.png");
 const searchImage = require("@/assets/images/iconSearch.png");
+const arrowBackImage = require("@/assets/images/icon_arrow_back.png");
+const arrowForwardImage = require("@/assets/images/icon_arrow_forward.png");
 function ItemSearch() {
   return (
     <div className="search">
@@ -51,20 +53,181 @@ function ItemSearch() {
                   <img src={itemImage} />
                   <span>아르카나 오브</span>
                 </td>
-                <td className="">
-                  <div>
+                <td>
+                  <div className="search-table-second">
                     <span>무기 데미지 증폭 100%</span>
                     <span>명중 +100</span>
                   </div>
                 </td>
                 <td>
-                  <div>
+                  <div className="search-table-all">
                     <img src={diamondImage} />
                     <span>1만 2000</span>
                   </div>
                 </td>
                 <td>
-                  <div>
+                  <div className="search-table-all">
+                    <img src={searchImage} />
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <img src={itemImage} />
+                  <span>아르카나 오브</span>
+                </td>
+                <td>
+                  <div className="search-table-second">
+                    <span>무기 데미지 증폭 100%</span>
+                    <span>명중 +100</span>
+                  </div>
+                </td>
+                <td>
+                  <div className="search-table-all">
+                    <img src={diamondImage} />
+                    <span>1만 2000</span>
+                  </div>
+                </td>
+                <td>
+                  <div className="search-table-all">
+                    <img src={searchImage} />
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <img src={itemImage} />
+                  <span>아르카나 오브</span>
+                </td>
+                <td>
+                  <div className="search-table-second">
+                    <span>무기 데미지 증폭 100%</span>
+                    <span>명중 +100</span>
+                  </div>
+                </td>
+                <td>
+                  <div className="search-table-all">
+                    <img src={diamondImage} />
+                    <span>1만 2000</span>
+                  </div>
+                </td>
+                <td>
+                  <div className="search-table-all">
+                    <img src={searchImage} />
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <img src={itemImage} />
+                  <span>아르카나 오브</span>
+                </td>
+                <td>
+                  <div className="search-table-second">
+                    <span>무기 데미지 증폭 100%</span>
+                    <span>명중 +100</span>
+                  </div>
+                </td>
+                <td>
+                  <div className="search-table-all">
+                    <img src={diamondImage} />
+                    <span>1만 2000</span>
+                  </div>
+                </td>
+                <td>
+                  <div className="search-table-all">
+                    <img src={searchImage} />
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <img src={itemImage} />
+                  <span>아르카나 오브</span>
+                </td>
+                <td>
+                  <div className="search-table-second">
+                    <span>무기 데미지 증폭 100%</span>
+                    <span>명중 +100</span>
+                  </div>
+                </td>
+                <td>
+                  <div className="search-table-all">
+                    <img src={diamondImage} />
+                    <span>1만 2000</span>
+                  </div>
+                </td>
+                <td>
+                  <div className="search-table-all">
+                    <img src={searchImage} />
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <img src={itemImage} />
+                  <span>아르카나 오브</span>
+                </td>
+                <td>
+                  <div className="search-table-second">
+                    <span>무기 데미지 증폭 100%</span>
+                    <span>명중 +100</span>
+                  </div>
+                </td>
+                <td>
+                  <div className="search-table-all">
+                    <img src={diamondImage} />
+                    <span>1만 2000</span>
+                  </div>
+                </td>
+                <td>
+                  <div className="search-table-all">
+                    <img src={searchImage} />
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <img src={itemImage} />
+                  <span>아르카나 오브</span>
+                </td>
+                <td>
+                  <div className="search-table-second">
+                    <span>무기 데미지 증폭 100%</span>
+                    <span>명중 +100</span>
+                  </div>
+                </td>
+                <td>
+                  <div className="search-table-all">
+                    <img src={diamondImage} />
+                    <span>1만 2000</span>
+                  </div>
+                </td>
+                <td>
+                  <div className="search-table-all">
+                    <img src={searchImage} />
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <img src={itemImage} />
+                  <span>아르카나 오브</span>
+                </td>
+                <td>
+                  <div className="search-table-second">
+                    <span>무기 데미지 증폭 100%</span>
+                    <span>명중 +100</span>
+                  </div>
+                </td>
+                <td>
+                  <div className="search-table-all">
+                    <img src={diamondImage} />
+                    <span>1만 2000</span>
+                  </div>
+                </td>
+                <td>
+                  <div className="search-table-all">
                     <img src={searchImage} />
                   </div>
                 </td>
@@ -74,7 +237,15 @@ function ItemSearch() {
         </div>
       </div>
       <div className="search__paging">
-
+        <div className="search__paging__area">
+          <img src={arrowBackImage} />
+          <span>1</span>
+          <span>2</span>
+          <span>3</span>
+          <span>4</span>
+          <span>5</span>
+          <img src={arrowForwardImage} />
+        </div>
       </div>
     </div>
   );
